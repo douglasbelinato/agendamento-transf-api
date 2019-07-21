4aa1e0e977eedebf2351f581cf3457ddd3063059
Douglas Belinato <douglas_belinato@hotmail.com>
2019-07-21 13:45:52 -0300

    Criação do modelo de dados e script para gerar massa de dados inicial.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+-- Populando usuários
+insert into usuario (email, senha, data_inclusao)
+    values ('douglas@email.com', '123', current_timestamp);
+
+-- Populando algumas transferências inicias para massa de dados
+insert into agendamentos_transferencia (conta_origem, conta_destino, valor, taxa, data_transferencia, data_agendamento, id_usuario)
+    values (12345, 45678, 100, 6, current_timestamp, parsedatetime('05-05-2019 18:47:52', 'dd-MM-yyyy hh:mm:ss'), 1);
+
+insert into agendamentos_transferencia (conta_origem, conta_destino, valor, taxa, data_transferencia, data_agendamento, id_usuario)
+    values (12345, 78945, 250, 6, current_timestamp, current_timestamp, 1);
+
+-- Populando regras parametrizadas dos tipos de transação para a cobrança das taxas
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('A', 1, 0, 0, 0, 999999.99, 3, 0.03, 'N', current_timestamp, 'S');
+
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('B', 1, 1, 10, 0, 999999.99, 12, 0, 'S', current_timestamp, 'S');
+
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('C', 1, 11, 20, 0, 999999.99, 0, 0.08, 'N', current_timestamp, 'S');
+
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('C', 2, 21, 30, 0, 999999.99, 0, 0.06, 'N', current_timestamp, 'S');
+
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('C', 3, 31, 40, 0, 999999.99, 0, 0.04, 'N', current_timestamp, 'S');
+
+insert into tipo_transacao (cod_tipo_transacao, faixa_prioridade, qtd_dias_de, qtd_dias_ate, valor_transf_de, valor_transf_ate, taxa_fixa, taxa_percentual, flag_fator_qtd_dias, data_inclusao, flag_ativa)
+    values ('C', 4, 41, 999, 100000.00, 999999.99, 0, 0.02, 'N', current_timestamp, 'S');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+-- Criando tabela de usuários
+create table usuario (
+    id bigint auto_increment not null,
+    email varchar(100) not null,
+    senha varchar(50) not null,
+    data_inclusao timestamp not null,
+    primary key (email)
+);
+
+-- Criando tabela de agendamentos de transferência
+create table agendamentos_transferencia (
+    id bigint auto_increment not null,
+    conta_origem int not null,
+    conta_destino int not null,
+    valor double not null,
+    taxa double not null,
+    data_transferencia date not null,
+    data_agendamento date not null,
+    id_usuario bigint not null,
+    primary key (id),
+    foreign key (id_usuario) references usuario(id)
+);
+
+-- Criando tabela de tipos de transação, responsável pela parametrização das taxas
+-- que serão cobradas de acordo com a quantidade de dias agendadas para a transferência
+-- e o valor da transferência
+create table tipo_transacao (
+    cod_tipo_transacao varchar(10) not null,
+    faixa_prioridade int not null,
+    qtd_dias_de int not null,
+    qtd_dias_ate int not null,
+    valor_transf_de double not null,
+    valor_transf_ate double not null,
+    taxa_fixa double not null,
+    taxa_percentual double not null,
+    flag_fator_qtd_dias varchar(1) not null,
+    data_inclusao timestamp not null,
+    flag_ativa varchar(1) not null,
+    primary key (cod_tipo_transacao, faixa_prioridade)
+);
